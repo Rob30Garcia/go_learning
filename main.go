1717327ea59dd0232eb09a61978b62869482fe88
Robert Garcia <robert.garcia.da.silva03@gmail.com>
2024-09-26 18:45:09 -0400

    feat: learning conditions in golang

--- main.go
+++ main.go
@@ -81,4 +81,41 @@ func main() {
 	fmt.Printf("numbersCopy = %v\n", numbersCopy)
 	fmt.Printf("length = %d\n", len(numbersCopy))
 	fmt.Printf("capacity = %d\n", cap(numbersCopy))
+
+	fmt.Println()
+	fmt.Println()
+	// Single-Case switch example
+	fmt.Println("Single-Case switch example")
+	day := 7
+
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	default:
+		fmt.Println("Not a weekday")
+	}
+
+	day = 7
+	switch day {
+	case 1, 3, 5:
+		fmt.Println("Odd weekday")
+	case 2, 4:
+		fmt.Println("Even weekday")
+	case 6, 7:
+		fmt.Println("Weekend")
+	default:
+		fmt.Println("Invalid day of day number")
+	}
 }
