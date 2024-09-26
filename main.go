30fdddb972689099c3790d6dec502f69e3c78315
Robert Garcia <robert.garcia.da.silva03@gmail.com>
2024-09-26 18:11:50 -0400

    feat: learning slice in go and testing concepts

--- main.go
+++ main.go
@@ -1,8 +1,84 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func main() {
 	fmt.Println(quote.Go())
+	fmt.Println()
+
+	// change elements of a slice
+	fmt.Println("change elements of a slice")
+	prices := []int{10, 20, 30}
+	prices[2] = 50
+	fmt.Println(prices[0])
+	fmt.Println(prices[2])
+	fmt.Println()
+
+	// Append elements to a slice
+	fmt.Println("Append elements to a slice")
+	myslice1 := []int{1, 2, 3, 4, 5, 6}
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	myslice1 = append(myslice1, 20, 21)
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+	fmt.Println()
+
+	// Append one slice to another slice
+	fmt.Println("Append one slice to another slice")
+	myslice1 = []int{1, 2, 3}
+	myslice2 := []int{4, 5, 6}
+	myslice3 := append(myslice1, myslice2...)
+
+	fmt.Printf("myslice3 = %v\n", myslice3)
+	fmt.Printf("length = %d\n", len(myslice3))
+	fmt.Printf("capacity = %d\n", cap(myslice3))
+	fmt.Println()
+
+	// Change the length of a slice
+	fmt.Println("Change the length of a slice")
+	arr1 := [6]int{9, 10, 11, 12, 13, 14} // An array
+	myslice1 = arr1[1:5]                  // slice array
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	fmt.Println("--change length by re-slicing the array--")
+	myslice1 = arr1[1:3] // change length by re-slicing the array
+	fmt.Printf("myslice1 =  %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	fmt.Println("--change length by appending items--")
+	myslice1 = append(myslice1, 20, 21, 22, 23) // change length by appending items
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+	fmt.Println()
+
+	// Memory Efficiency
+	fmt.Println("Memory efficiency")
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	// Original slice
+	fmt.Printf("numbers = %v\n", numbers)
+	fmt.Printf("length = %d\n", len(numbers))
+	fmt.Printf("capacity = %d\n", cap(numbers))
+
+	// Create copy with only needed numbers
+	fmt.Println("Create copy with only needed numbers")
+	neededNumbers := numbers[:len(numbers)-10]
+	numbersCopy := make([]int, len(neededNumbers))
+	fmt.Println(numbersCopy)
+	copy(numbersCopy, neededNumbers)
+
+	fmt.Printf("numbersCopy = %v\n", numbersCopy)
+	fmt.Printf("length = %d\n", len(numbersCopy))
+	fmt.Printf("capacity = %d\n", cap(numbersCopy))
 }
